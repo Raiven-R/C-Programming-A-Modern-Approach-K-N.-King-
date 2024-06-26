04611f61cff366e6063aa41346f08e6a0a54c69f
Raiven <147449858+Raiven-arch@users.noreply.github.com>
2024-06-26 16:26:03 +0600

    Add files via upload

---
+++ Chapter 6/Projects/2_for.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (){
+
+    int m, n;
+
+    printf("Enter 1st number: ");
+    scanf("%d", &m);
+    printf("Enter 2nd number: ");
+    scanf("%d", &n);
+
+    for(; n != 0 ;){
+       
+        int remainder = m % n;
+        m = n;
+        n = remainder;
+    }
+
+
+    printf("Greatest common divisor: %d", m);
+
+    return 0;
+}
